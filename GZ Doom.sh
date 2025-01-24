07e2b0e83a93742587a1ed4737cb88b7a8a91c09
Jeod <47716344+JeodC@users.noreply.github.com>
2025-01-23 19:05:06 -0500

    Initial commit

---
+++ GZ Doom.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Variables
+GAMEDIR="/$directory/ports/gzdoom"
+HEIGHT=$DISPLAY_HEIGHT
+WIDTH=$DISPLAY_WIDTH
+CONFIG="$GAMEDIR/cfg/gzdoom.ini"
+ARGS="+gl_es 1 +vid_preferbackend 3 +cl_capfps 0 +vid_fps 0"
+
+# CD and set permissions
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+$ESUDO chmod +x -R $GAMEDIR/*
+
+# Create config dir
+bind_directories ~/".config/gzdoom" "$GAMEDIR/cfg"
+
+# Exports
+export LD_LIBRARY_PATH="$GAMEDIR/libs:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+
+# Run launcher
+chmod +xr ./love
+$GPTOKEYB "love" &
+./love launcher
+
+# Read line from selected_game.txt
+while IFS= read -r line; do
+    if [ -z "$FOLDER" ]; then
+        FOLDER="$line"  # First line is FOLDER
+    fi
+done < selected_game.txt
+
+# Cleanup launcher
+if [ -f selected_game.txt ]; then
+    rm -rf selected_game.txt
+    pm_finish
+else
+    exit
+fi
+
+# Modify resolution in config file
+sed -i "s/^vid_defheight=[0-9]\+/vid_defheight=$HEIGHT/" "$CONFIG"
+sed -i "s/^vid_defwidth=[0-9]\+/vid_defwidth=$WIDTH/" "$CONFIG"
+
+# If Exit chosen from launcher, quit
+if [[ $FOLDER == "Exit" ]]; then
+    exit
+fi
+
+# Build args by reading the load.txt files in the folder chosen
+if [ -n "$FOLDER" ]; then
+    dos2unix "$FOLDER/.load.txt" >/dev/null 2>&1
+    TMP=$IFS
+    while IFS== read -r key value; do
+        case "$key" in
+            DATA)
+                ARGS="$ARGS -iwad $value.WAD"
+                ;;
+            PK3|PK3_1|PK3_2|PK3_3|PK3_4)
+                ARGS="$ARGS -file $GAMEDIR/mods/$value"
+                ;;
+        esac
+    done < "${FOLDER}/.load.txt"
+    IFS=$TMP
+fi
+
+# Run game
+echo "[LOG]: Running with args: ${ARGS}"
+$GPTOKEYB "gzdoom" -c "gzdoom.gptk" & 
+pm_platform_helper "$GAMEDIR/gzdoom"
+./gzdoom $ARGS
+
+# Cleanup
+pm_finish
---
+++ README.md
@@ -0,0 +1,46 @@
+# GZ Doom -- A PortMaster implementation of GZDoom to play Doom WADs
+
+## Installation
+This port comes with the free Doom shareware and Freedoom1 and Freedoom2 WAD files. GZDoom supports many WAD files. See the [wiki page](https://zdoom.org/wiki/IWAD) for details.
+
+To use them, place your WAD files in `ports/gzdoom/data`.
+
+## Play
+To use addon mods, place `.pk3` files in `ports/gzdoom/mods` and list the `.pk3` files to load in `.load.txt` for the relevant game.
+
+## Mod
+The launcher lists folders as games and looks for `.load.txt` files inside them. It uses their information to construct arguments passed to gzdoom, and will not display menu options for any games that are missing data. To create a `.load.txt` file, open a text editor and add the following:
+
+- `PATH` - This is always `./data`
+- `DATA` - File name of the WAD to use
+- `PK3_#` - Any `.pk3` files to load after the data, can use up to four
+
+Follow this example `Doom/.load.txt` which launches vanilla Doom:
+
+```
+PATH=./data
+DATA=DOOM
+-- end --
+```
+
+This example `Legend of Doom/.load.txt` launches the mod The Legend of Doom:
+
+```
+PATH=./data
+DATA=FREEDOOM2
+PK3_1=LegendOfDoom-1.1.0.pk3
+PK3_2=LoDMusicLoops.pk3
+-- end --
+```
+
+Since we gave the mod its own subfolder and `.load.txt` file, it appears in the launcher as its own option despite using shared data files.
+
+## Building
+There is no need to build, as [GZDoom](https://github.com/ZDoom/gzdoom) offers `.deb` releases.
+
+## Thanks
+id Software -- Original game  
+GZDoom Team -- GZDoom, see license file for individual contributions
+Andrew Hushult -- The [music](https://www.youtube.com/watch?v=Yctbs7A4KHk) used for the launcher  
+Slayer366 -- Original GZDoom push and port assistance  
+PortMaster Discord -- Testers
